83e49d61f0b2f312d8e29ce9fe7555b050a1d686
Vikas V R <vikaspuranik98@gmail.com>
2024-07-21 22:04:55 +0530

    2nd commit for this project

---
+++ src/Java14July2024/data_Types.java
@@ -0,0 +1,23 @@
+package Java14July2024;
+
+public class data_Types
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Data Types");
+        byte b=10;
+        short s=12;
+        char c='A';
+        int i=100;
+        long l=9876543210L; // Note : For Long data type you should compulsory end the value by 'l' or 'L'
+        float f=3.14f; // Same thing applies to float also
+        double d=3.14234556677;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+    }
+}
